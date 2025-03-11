0fc613c6c9aaf8af4084649c9eca8196f37bc49a
Kevin Szuchet <31735779+kevinszuchet@users.noreply.github.com>
2025-03-11 12:48:49 +0100

    feat: Enhance tracking (#8)
    * feat: Avoid tracking bots
    
    * feat: Change site title + call analytycs page immeditely after load

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,11 @@ import { getAnalytics } from "./modules/analytics/segment"
 import "./modules/analytics/snippet"
 
 const analytics = getAnalytics()
-analytics.load(config.get("SEGMENT_API_KEY"))
-analytics.ready(() => {
-  console.log("Ready!")
+
+if (analytics) {
+  analytics.load(config.get("SEGMENT_API_KEY"))
   analytics.page()
-})
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
