c867dc119e7c945211aa025f616cff3b43cb6ea6
Aga <aga@besz.me>
2025-09-01 15:44:01 +0100

    chore: update launcher download urls in DownloadBtn.tsx (#23)
    * chore: update launcher download urls in DownloadBtn.tsx
    
    Signed-off-by: Aga <aga@besz.me>
    
    * Update pull-request.yml
    
    Signed-off-by: Aga <aga@besz.me>
    
    ---------
    
    Signed-off-by: Aga <aga@besz.me>

--- src/components/DownloadBtn/DownloadBtn.tsx
+++ src/components/DownloadBtn/DownloadBtn.tsx
@@ -4,9 +4,9 @@ import { styled } from "styled-components"
 import { useAdvancedUserAgentData } from "../../hooks/useAdvancedUserAgentData"
 
 enum DownloadLinks {
-  MAC_ARM64 = "https://explorer-artifacts.decentraland.org/launcher/dcl/Decentraland%20Launcher-mac-arm64.dmg",
-  MAC_X64 = "https://explorer-artifacts.decentraland.org/launcher/dcl/Decentraland%20Launcher-mac-x64.dmg",
-  WIN_X64 = "https://explorer-artifacts.decentraland.org/launcher/dcl/Decentraland%20Launcher-win-x64.exe",
+  MAC_ARM64 = "https://explorer-artifacts.decentraland.org/launcher-rust/Decentraland_installer.dmg",
+  MAC_X64 = "https://explorer-artifacts.decentraland.org/launcher/dcl/Decentraland%20Outdated-mac-x64.dmg",
+  WIN_X64 = "https://explorer-artifacts.decentraland.org/launcher-rust/Decentraland_installer.exe",
   UNKNOWN = "",
   MOBILE_REDIRECT = "https://decentraland.org/download/",
 }
